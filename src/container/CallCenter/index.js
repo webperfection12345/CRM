6b4ce8f48390d3cc9dc9ac068c2f0dac6ba5acf5
Dev Me <devme@Devs-Mac-mini-2.local>
2023-07-07 16:11:54 +0530

    Working on Call Center Screen

--- src/container/CallCenter/index.js
+++ src/container/CallCenter/index.js
@@ -21,36 +21,32 @@ const CallCenter = () => {
       <Image style={styles.iconsmain}
             source={require("../../../assets/callblack.png")}
           ></Image>
-        <Text>Phone</Text>
+         <Text style={styles.textcall}>Phone</Text>
         <Text>0</Text>
       </View>
     <View style={styles.whitephone}>
       <Image style={styles.iconsmain} 
             source={require("../../../assets/blackchat.png")}
           ></Image>
-          <Text>Call</Text>
+          <Text style={styles.textcall}>Call</Text>
         <Text>4</Text>
           </View>
           </View>
          <Text style={styles.centertext}>Call Center</Text>
-         <View>
+
           <View styles={styles.covericons}>
-         <TouchableOpacity
-          style={styles.plusiconcover}
-        >
+         <TouchableOpacity>
           <Image style={styles.plusimage}
             source={require("../../../assets/notificationwhite.png")}
           ></Image>
         </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.plusiconcover}
-        >
+        <TouchableOpacity>
           <Image style={styles.plusimage}
             source={require("../../../assets/notificationwhite.png")}
           ></Image>
         </TouchableOpacity>
         </View>
-         </View>
+        
       </View>
       </SafeAreaView>
 
@@ -61,16 +57,17 @@ const styles = StyleSheet.create({
   headermain:{flexDirection:"row",justifyContent:"space-between",backgroundColor:Colors.PrimaryColor,alignItems:"center",
 paddingHorizontal:12,paddingVertical:6},
 leftheader:{alignItems:"center",flexDirection:"row"},
-questext:{color:Colors.white,fontSize:14,},
-whitephone:{backgroundColor:Colors.white,borderRadius:5,alignItems:"center",justifyContent:"center",paddingHorizontal:8,paddingVertical:5,margin:3,width:40
+questext:{color:Colors.white,fontSize:12,},
+whitephone:{backgroundColor:Colors.white,borderRadius:5,alignItems:"center",justifyContent:"center",paddingHorizontal:4,paddingVertical:5,margin:3,width:40
 },
-iconsmain:{width:15,height:15},
-centertext:{fontSize: 19, fontWeight: "bold", color: Colors.white },
+iconsmain:{width:13,height:13},
+centertext:{fontSize: 15, fontWeight: "bold", color: Colors.white },
 plusimage: {
-  height: 17,
-  width: 17,
+  height: 15,
+  width: 15,
   resizeMode: "contain",
   tintColor: Colors.white,
 },
-covericons:{flexDirection:"row",alignItems:"center"}
+covericons:{flexDirection:"row",alignItems:"center"},
+textcall:{fontSize:12}
 });
